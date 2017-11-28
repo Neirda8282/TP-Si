c35746dcf1ccdd472f42838d13a0d0e2874d0b2e
Neirda8282 <adrien8282@hotmail.fr>
2017-11-28 12:00:45 +0100

    up

--- comax/comax/comax.py
+++ comax/comax/comax.py
@@ -1,4 +1,20 @@
 from math import *
 from pylab import *
+U=[2.475,2.475,2.475,2.475]
+I=[4.700,4.900,4.900,4.900]
+omegam=[250,75,125,35]
+f=(4+1.1)*9.81
+v=[108*(10**-3)*(1/60)*omega for omega in omegam]
+Pelec=[]
+Ps=[]
+n=[]
+for i in range (len(U)):
+    Pelec.append(U[i]*I[i])
+    
+    Ps.append(v[i]*f)
+    n.append(Pelec[i]/Ps[i])
+    
+plot(Pelec,omegam,'r*',Ps,omegam,'g+',n,omegam,'^')
+show()
 
 
